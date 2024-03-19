44be184dd45dead45918a96664ecbba5bc177a4b
admajagit <128940180+admajagit@users.noreply.github.com>
2024-03-19 09:14:57 +0700

    perubahan drastis change major

--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
 module dasar
 
 go 1.22.0
+
--- main.go
+++ main.go
@@ -3,41 +3,16 @@ package main
 // import package error
 import (
 	"fmt"
+
+	"github.com/admajagit/latku"
+	"github.com/admajagit/latku/biasa"
 )
-
-// membuat custom error
-// error menmapung return string
-
-// jika kit amnegakses sebuah struct otomatis langsung dilempar ke implementasi
-type Errorku struct {
-	Pesan string
-}
-
-
-
-func (Jo Errorku) Error() string {
-	return Jo.Pesan
-}
-
-
-
-func Pembagi(pertama int , kedua int) (int , error) {
-	if kedua == 0 {
-
-		// 
-		return 0,Errorku{
-			Pesan: "tidak bisa coy",
-
-		}
-	}
-
-	return pertama/kedua,nil
-}
 
 func main()  {
 
-	alok,err := Pembagi(100,0)
-
-	fmt.Println(alok)
-	fmt.Println(err)
+	fmt.Println("hallo cik")
+	latihanmodule.Hallo()
+	latihanmodule.Kita()
+	biasa.Alok()
+	
 }
